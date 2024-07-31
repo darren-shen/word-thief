280115ca1cfaad50cc79ec5d371ae6f86800fa37
darren-shen <darrenshen@berkeley.edu>
2024-07-31 10:51:20 -0700

    local single player functional

--- backend/gamestate.py
+++ backend/gamestate.py
@@ -3,6 +3,8 @@ from flask_socketio import SocketIO, emit
 from flask_cors import CORS
 from collections import defaultdict
 from random import sample, randint
+from datetime import datetime
+from time import sleep
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
@@ -10,68 +12,153 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 game_state = {
-    "letters": [],
-    "letter_counts": [0] * 26,
+    "letters": "",
+    "letter_counts": defaultdict(int),
     "players": []
 }
 
 agent_vocabulary = open('./wordlist.txt', 'r').read().splitlines()
+alphabet = "AAAAAAAAAAAAABBBCCCDDDDDDEEEEEEEEEEEEEEEEEEFFFGGGGHHHIIIIIIIIIIIIJJKKLLLLLMMMNNNNNNNNOOOOOOOOOOOPPPQQRRRRRRRRRSSSSSSTTTTTTTTTUUUUUUVVVWWWXXYYYZZ"
 
+
+# TODO: update how speed is calculated
 @socketio.on('initialize')
 def initialize(data):
     global game_state
-    num_agents, num_players = data.get('num_agents', 2), data.get('num_players', 1)
+    num_agents, num_players, letter_frequency, max_letters = data.get('num_agents', 2), data.get('num_players', 1), data.get('letter_frequency', 5), data.get('max_letters', 10)
     game_state = {
-        "letters": [],
-        "letter_counts": [0] * 26,
-        "players": {}
+        "letters": "",
+        "letter_counts": defaultdict(int),
+        "letter_timer": letter_frequency,
+        "players": {},
+        "ended": False
     }
     for i in range(num_players):
         game_state['players'][i] = {"agent": False, "words": []}
     for i in range(num_agents):
-        game_state['players'][num_players + i] = {"agent": True, "words": [], "vocab": sample(agent_vocabulary, randint(1000, 10000))}
-    emit('game_state', game_state, broadcast=True)
-
-@socketio.on('add_word')
-def add_word(data):
-    newWord, id = data.get('word'), data.get('id')
-    game_state['players'][id].append(newWord)
-    emit('game_state', game_state, broadcast=True)
+        speed = randint(6, 10)
+        game_state['players'][num_players + i] = {"agent": True, "words": [], "vocab": sample(agent_vocabulary, randint(1000, len(agent_vocabulary))), "speed": speed, "next_move": speed}
     
-
-@socketio.on('remove_word')
-def remove_word(data):
-    newWord, id = data.get('word'), data.get('id')
-    if newWord in game_state['players'][id]:
-        game_state['players'][id].remove(newWord)
     emit('game_state', game_state, broadcast=True)
 
-@socketio.on('add_letters')
-def add_letters(data):
-    new_letters = data.get('letters', '')
-    game_state['letters'] += new_letters
+    prev_time = datetime.now()
+    while not game_state["ended"]:
+        curr_time = datetime.now()
+        game_state["letter_timer"] -= (curr_time - prev_time).total_seconds() 
+        if game_state["letter_timer"] < 0 and len(game_state["letters"]) < max_letters:
+            add_random_letter()
+            game_state["letter_timer"] = letter_frequency
+        
+        for id in game_state["players"]:
+            if game_state["players"][id]["agent"]:
+                game_state["players"][id]["next_move"] -= (curr_time - prev_time).total_seconds()
+                if game_state["players"][id]["next_move"] < 0:
+                    agent_move(id)
+                    game_state["players"][id]["next_move"] = game_state["players"][id]["speed"]
+        sleep(.2)
+        prev_time = curr_time
+        emit('game_state', game_state, broadcast=True)
+
+
+@socketio.on('process_packet') 
+def process_packet(data):
+    print(data)
+    if "add_word" in data:
+        add_word(data.get("add_word"), int(data.get("add_id")))
+    if "remove_word" in data:
+        remove_word(data.get("remove_word"), int(data.get("remove_id")))
+
+    if "add_letters" in data:
+        add_letters(data.get("add_letters"))
+    if "remove_letters" in data:
+        remove_letters(data.get("remove_letters"))
+
+    emit('game_state', game_state, broadcast=True)
+
+
+def add_word(new_word, id):
+    game_state['players'][id]["words"].append(new_word)
+    
+def remove_word(new_word, id):
+    if new_word in game_state['players'][id]["words"]:
+        game_state['players'][id]["words"].remove(new_word)
+
+def add_letters(new_letters):
     for l in new_letters:
-        game_state['letter_counts'][ord(l) - 65] += 1
-    emit('game_state', game_state, broadcast=True)
+        game_state['letter_counts'][l] += 1
+    game_state['letters'] += new_letters
 
-@socketio.on('remove_letters')
-def remove_letters(data):
-    new_letters = data.get('letters', '')
+def remove_letters(new_letters):
     new_letter_counts = defaultdict(int)
     for l in new_letters:
         new_letter_counts[l] += 1
     temp = ""
     for l in game_state['letters']:
-        if new_letter_counts[ord(l) - 65] > 0:
-            new_letter_counts[ord(l) - 65] -= 1
+        if new_letter_counts[l] > 0:
+            new_letter_counts[l] -= 1
+            game_state['letter_counts'][l] -= 1
         else:
             temp += l
-            game_state['letter_counts'] [ord(l) - 65] -= 1
-    emit('game_state', game_state, broadcast=True)
+    game_state['letters'] = temp
 
 @app.route('/http-call')
 def http_call():
-    return jsonify(game_state)
+    print('hi')
+    data = {'data':'This text was fetched using an HTTP call to server on render'}
+    return jsonify(data)
+        
+def add_random_letter():
+    add_letters(alphabet[randint(0, len(alphabet) - 1)])
+
+def contains(smallerCount, largerCount):
+    for letter in smallerCount:
+        if smallerCount[letter] > largerCount[letter]:
+            return False
+    return True
+
+def same(smallerCount, largerCount):
+    for letter in smallerCount:
+        if smallerCount[letter] != largerCount[letter]:
+            return False
+    for letter in largerCount:
+        if smallerCount[letter] != largerCount[letter]:
+            return False
+    return True
+
+def agent_move(id):
+    curr = game_state["players"][id]
+    order = sample(range(len(game_state["players"]) + 1), len(game_state["players"]) + 1)
+    for listId in order:
+        if listId == len(game_state["players"]):
+            for vocab_word in curr["vocab"]:
+                vocab_word_count = defaultdict(int)
+                for l in vocab_word:
+                    vocab_word_count[l] += 1
+                if contains(vocab_word_count, game_state["letter_counts"]):
+                    add_word(vocab_word, id)
+                    remove_letters(vocab_word)
+                    return
+        else:            
+            for vocab_word in curr["vocab"]:
+                vocab_word_count = defaultdict(int)
+                for l in vocab_word:
+                    vocab_word_count[vocab_word] += 1
+                for word in game_state["players"][listId]["words"]:
+                    combined_letter_count = game_state["letter_counts"].copy()
+                    word_count = defaultdict(int)
+                    for l in word:
+                        combined_letter_count[l] += 1
+                        word_count[l] += 1
+                    if contains(vocab_word_count, combined_letter_count) and contains(word_count, vocab_word_count) and not same(word_count, vocab_word_count):
+                        for l in word_count:
+                            combined_letter_count[l] -= word_count[l]
+                        add_word(vocab_word, id)
+                        remove_word(word, listId)
+                        remove_letters(combined_letter_count)
+                        return
+
+# TODO: add win condition
+
 
 if __name__ == '__main__':
     socketio.run(app, port=5001, debug=True)
--- word-thief/src/components/GameBoard.jsx
+++ word-thief/src/components/GameBoard.jsx
@@ -1,63 +1,46 @@
 "use client"
 import { useState, useEffect, useRef } from 'react'
 import { useWordChecker } from 'react-word-checker'
-import { words } from 'popular-english-words'
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:5001');
+const socket = io('http://localhost:5001/', {
+  transports: ["websocket"],
+  cors: {
+    origin: "http://localhost:3000/",
+  },
+  }
+)
+
 const MAX_LETTERS = 10
 
 const GameBoard = ({ wordsToWin, numOpponents }) => {
-    const [letters, setLetters] = useState('')
-    const letterCounts = useRef({});
+    const [gameState, setGameState] = useState({
+                              "letters": "",
+                              "letter_counts": {},
+                              "letter_timer": 5,
+                              "players": {0: {"agent": false, "words": []}, 1: {"agent": true, "words": [], "vocab": [], "speed": 6, "next_move": 6}, 2:{"agent": true, "words": [], "vocab": [], "speed": 6, "next_move": 6}},
+                              "ended": false
+                          })
+    const { wordExists } = useWordChecker("en")
     const [currWord, setWord] = useState('')
-    const currWordCounts = useRef({});
-    const [yourWords, setYourWords] = useState([])
-
-    const [gameState, setGameState] = useState({})
-    const { wordExists } = useWordChecker("en");
-    
-
-    /* INITIALIZE ENEMIES */
-
-    const makeAgents = (numOpponents) => {
-      const agents = []
-      for (let i = 1; i <= numOpponents; i++) {
-        const next = { id: i, words: ['TEST'], vocab: words.getMostPopular(500 + 2000 * (i - 1)) }
-        next.vocab.sort(() => Math.random() - 0.5)
-        next.vocab = next.vocab.map(function(x){ return x.toUpperCase() })
-        agents.push(next)
-      }
-      return agents
-    };
-
-    // const [agents, setAgents] = useState(() => makeAgents(numOpponents))
-
+    const currWordCounts = useRef({})
+    const id = 0
 
     /* INITIALIZE BACKEND */
+    let initialized = false
     useEffect(() => {
-      const initializeGame = async () => {
-        const gameState = await axios.post('http://localhost:5000/initialize', numOpponents)
-        setLetters(gameState.data.letters)
-        setYourWords(gameState.data.players[0].words)
+      if (!initialized) {
+        socket.emit('initialize', { "num_agents": 4, "num_players": 1, "letter_frequency": 2, "max_letters": 10 })
+        initialized = true
       }
-      initializeGame()
+      socket.on('game_state', (gameState) => {
+        setGameState(gameState)
+      })
+
+      return () => {
+        socket.off('game_state')
+      }
     }, [numOpponents])
-
-
-    /* REMOVE LETTERS FROM CURRENT GIVEN NEW COUNT */
-    const removeLetters = (letters, removeCount) => {
-      let newLetters = ""
-      for (const letter of letters) {
-        if (removeCount[letter] > 0) {
-          letterCounts.current[letter] -= 1
-          removeCount[letter] -= 1
-        } else {
-          newLetters += letter
-        }
-        setLetters(newLetters)
-      }
-    }
 
 
     /* KEY PRESSES FOR INPUT FIELD */
@@ -105,92 +88,39 @@ const GameBoard = ({ wordsToWin, numOpponents }) => {
       return true
     }
 
-    const generateRandomPermutation = (n) => {
-      let permutation = Array
-          .from({ length: n }, (_, i) => i );
-      permutation
-          .sort(() => Math.random() - 0.5);
-      return permutation
-    }
-
-    const valid = ( attemptedWord ) => {
-      if (attemptedWord.length < 3) {
-        return { valid: false }
-      }
-      const wordCounts = {}
-      for (const letter of attemptedWord) {
-        wordCounts[letter] = (wordCounts[letter] || 0) + 1
-      }
-
-      const checkOrder = generateRandomPermutation(numOpponents + 2)
-
-      for (const id of checkOrder) {
-          if (check(letterCounts.current, wordCounts)) {
-            console.log(letterCounts.current, wordCounts)
-            return { valid: true, lettersUsed: wordCounts }
-          }
-        // } else if (id == 0) {
-        //   for (const agentWord of yourWords) {
-        //     const combinedCounts = { ...letterCounts.current }
-        //     const chosenCounts = {}
-        //     for (const letter of agentWord) {
-        //       combinedCounts[letter] = (combinedCounts[letter] || 0) + 1
-        //       chosenCounts[letter] = (chosenCounts[letter] || 0) + 1
-        //     }
-        //     if (check(combinedCounts, wordCounts) && check(wordCounts, chosenCounts) && !same(chosenCounts, wordCounts)) {
-        //       const finalCounts = { ...wordCounts }
-        //       for (const letter of agentWord) {
-        //         finalCounts[letter] -= 1
-        //       }
-        //       return { valid: true, lettersUsed: finalCounts, agentUsed: id, wordUsed: agentWord }
-        //     } 
-        //   }
-        // } else {
-        //   for (const agentWord of agents[id - 1].words) {
-        //     const combinedCounts = { ...letterCounts.current }
-        //     const chosenCounts = {}
-        //     for (const letter of agentWord) {
-        //       combinedCounts[letter] = (combinedCounts[letter] || 0) + 1
-        //       chosenCounts[letter] = (chosenCounts[letter] || 0) + 1
-        //     }
-        //     if (check(combinedCounts, wordCounts) && check(wordCounts, chosenCounts) && !same(chosenCounts, wordCounts)) {
-        //       const finalCounts = { ...wordCounts }
-        //       for (const letter of agentWord) {
-        //         finalCounts[letter] -= 1
-        //       }
-        //       return { valid: true, lettersUsed: finalCounts, agentUsed: id, wordUsed: agentWord }
-        //     } 
-        //   }
-        // } 
-      }
-      return { valid: false }
-    }
-
 
     /* MOUSE CLICKS TO FINISH WORDS */
 
     const lettersClick = () => {
+      console.log(currWord)
       if (!(wordExists(currWord)) || currWord.length < 3){
         return
       }
+      console.log('yay')
+      console.log(gameState["letter_counts"])
       for (const letter in currWordCounts.current) {
-        if (currWordCounts.current[letter] > (letterCounts.current[letter] || 0)) {
+        if (currWordCounts.current[letter] > gameState["letter_counts"][letter]) {
           return 
         }
       }
-      setYourWords([...yourWords, currWord])
+
+      socket.emit('process_packet', { "add_word": currWord, "add_id": id, "remove_letters": currWord })
+      socket.on('game_state', (gameState) => {
+        setGameState(gameState)
+      })
+
       setWord('')
-      removeLetters(letters, currWordCounts.current)
       currWordCounts.current = {}
     }
 
-    const wordListClick = (wordList) => {
+    const wordListClick = (clickId, wordList) => {
+      clickId = parseInt(clickId)
+      console.log(currWord)
       if (!(wordExists(currWord)) || currWord.length < 3){
         return
       }
       for (const word of wordList) {
-        console.log(letterCounts.current)
-        const combinedCounts = { ...letterCounts.current }
+        const combinedCounts = { ...gameState["letter_counts"] }
         const chosenCounts = {}
         for (const letter of word) {
           combinedCounts[letter] = (combinedCounts[letter] || 0) + 1
@@ -199,18 +129,23 @@ const GameBoard = ({ wordsToWin, numOpponents }) => {
         console.log(combinedCounts)
         console.log(currWordCounts.current)
         if (check(combinedCounts, currWordCounts.current) && check(currWordCounts.current, chosenCounts) && !same(currWordCounts.current, chosenCounts)) {
-          const wordIndex = wordList.indexOf(word);
-          if (wordIndex !== -1) {
-            wordList.splice(wordIndex, 1);
-          }
           const usedLetterCounts = { ...currWordCounts.current }
           for (const letter of word) {
             usedLetterCounts[letter] -= 1
           }
+          let remove = ""
+          for (const letter in usedLetterCounts) {
+            for (let i = 0; i < usedLetterCounts[letter]; i++) {
+              remove += letter
+            }
+          }
 
-          removeLetters(letters, usedLetterCounts)
+          socket.emit('process_packet', { "add_word": currWord, "add_id": id, "remove_word": word, "remove_id": clickId, "remove_letters": remove })
+          socket.on('game_state', (gameState) => {
+            setGameState(gameState)
+          })
 
-          setYourWords([...yourWords, currWord])
+
           setWord('')
           currWordCounts.current = {}
           return
@@ -219,110 +154,14 @@ const GameBoard = ({ wordsToWin, numOpponents }) => {
     }
 
 
-    /* GENERATE RANDOM LETTERS */
-
-    useEffect(() => {
-      const addRandomLetter = () => {
-        const ALPHABET = "AAAAAAAAAAAAABBBCCCDDDDDDEEEEEEEEEEEEEEEEEEFFFGGGGHHHIIIIIIIIIIIIJJKKLLLLLMMMNNNNNNNNOOOOOOOOOOOPPPQQRRRRRRRRRSSSSSSTTTTTTTTTUUUUUUVVVWWWXXYYYZZ"
-        if (letters.length < MAX_LETTERS) {
-          const newLetter = ALPHABET[Math.floor(Math.random() * ALPHABET.length)]
-          setLetters(letters + newLetter)
-          letterCounts.current[newLetter] = (letterCounts.current[newLetter] || 0) + 1
-        } else {
-          clearInterval(intervalId)
-        }
-      }
-
-      const intervalId = setInterval(addRandomLetter, 1200);
-
-      return () => clearInterval(intervalId)
-    }, [letters]);
-
-
-    /* ENEMY MAKES RANDOM MOVES */
-
-    /*
-    useEffect(() => {
-      const agentMoves = () => {
-        for (const vocabWord of agents[0].vocab) {
-          const curr = valid(vocabWord)
-          if (curr.valid) {
-            console.log(curr)
-            console.log(vocabWord)
-            if (curr.agentUsed) {
-              const wordIndex = agents[curr.agentUsed - 1].words.indexOf(curr.wordUsed);
-              if (wordIndex !== -1) {
-                agents[curr.agentUsed - 1].words.splice(wordIndex, 1);
-              }
-            }
-            
-            console.log(letters)
-            removeLetters(letters, curr.lettersUsed)
-
-            agents[0].words = [ ...agents[0].words, vocabWord ]
-            return
-          }
-        }
-      }
-
-      const intervalId = setInterval(agentMoves, 1000);
-
-      return () => clearInterval(intervalId)
-    }, [letters]);
-
-    /*
-
-    useEffect(() => {
-      const agentMoves = (agent) => {
-        console.log(agent)
-        for (const vocabWord of agent.vocab) {
-          const curr = valid(vocabWord)
-          if (curr.valid) {
-            console.log(vocabWord)
-            if (curr.agentUsed) {
-              const wordIndex = agents[id - 1].words.indexOf(curr.wordUsed);
-              if (wordIndex !== -1) {
-                agents[id - 1].words.splice(wordIndex, 1);
-              }
-            }
-            
-            let newLetters = ""
-            for (const letter of letters) {
-              if (curr.lettersUsed[letter] > 0) {
-                letterCounts.current[letter] -= 1
-                curr.lettersUsed[letter] -= 1
-              } else {
-                newLetters += letter
-              }
-              setLetters(newLetters)
-            }
-
-            agent.words = [ ...agent.words, vocabWord]
-          }
-        }
-      }
-
-      const agentIntervalIds = agents.map((agent) => {
-        return setInterval(() => agentMoves(agent), Math.random() * 5000 + 3000) // Random interval between 3 to 8 seconds
-      })
-
-      return () => {
-        agentIntervalIds.forEach(clearInterval);
-      };
-    }, [agents]);
-
-
-
-    /* NEXT STEPS: 2. enemy ai to take random words, random timing between takes (to simulate different difficulties, 
-    make "dictionaries" of words that each difficulty ai can use) 3. win condition 4. formatting! make things look nice */ 
-
+    /* TODO: add formatting! :D */
 
     return (
       <div id="gameBoard" tabIndex="0">
         <div className="flex flex-row justify-center">
-          <button onClick={() => lettersClick()}>     
+          <button onClick={lettersClick}>
             <p className="mt-4 p-2">LETTERS</p>
-            {letters.split('').map((char, i) => (
+            {gameState["letters"]?.split("").map((char, i) => (
               <span key={i} className="border border-black pl-1.5 pr-1.5 pt-0.5 pb-0.5 m-1 rounded-md inline-block">
                 {char}
               </span>
@@ -331,25 +170,26 @@ const GameBoard = ({ wordsToWin, numOpponents }) => {
         </div>
         <div className="flex flex-row justify-center space-x-10">
           <div key={0}>
-            <button onClick={() => wordListClick(yourWords)}>
+            <button onClick={() => wordListClick(id, gameState["players"][id]["words"])}>
               <p className="mt-4 p-2 justify-center flex flex-row">YOUR WORDS</p>
-              {yourWords.map((word, i) => (
-                <div key={i} className="flex flex-row justify-center" >
-                  {word.split('').map((char, j) => (
-                    <div key={j} className="border border-black pl-1.5 pr-1.5 pt-0.5 pb-0.5 m-1 rounded-md">
-                      {char}
-                    </div>
-                  ))}
-                </div>
-            ))}
+              {gameState["players"][id]["words"]?.map((word, i) => (
+              <div key={i} className="flex flex-row justify-center">
+                {word.split('').map((char, j) => (
+                  <div key={j} className="border border-black pl-1.5 pr-1.5 pt-0.5 pb-0.5 m-1 rounded-md">
+                    {char}
+                  </div>
+                ))}
+              </div>
+              ))}
             </button>
           </div>
-          {agents.map((agent) => (
-            <div key={agent.id}>
-              <button onClick={() => wordListClick(agent.words)}>
-                <p className="mt-4 p-2 justify-center flex flex-row">ENEMY {agent.id}</p>
-                {agent.words.map((word, i) => (
-                  <div key={i} className="flex flex-row justify-center" >
+          {Object.keys(gameState["players"] || {}).map((playerId) => (
+          gameState["players"][playerId]["agent"] && (
+            <div key={playerId}>
+              <button onClick={() => wordListClick(playerId, gameState.players[playerId]?.words || [])}>
+                <p className="mt-4 p-2 justify-center flex flex-row">ENEMY {playerId}</p>
+                {gameState["players"][playerId]["words"]?.map((word, i) => (
+                  <div key={i} className="flex flex-row justify-center">
                     {word.split('').map((char, j) => (
                       <div key={j} className="border border-black pl-1.5 pr-1.5 pt-0.5 pb-0.5 m-1 rounded-md">
                         {char}
@@ -359,7 +199,8 @@ const GameBoard = ({ wordsToWin, numOpponents }) => {
                 ))}
               </button>
             </div>
-          ))}
+          )
+        ))}
         </div>
 
         <input
