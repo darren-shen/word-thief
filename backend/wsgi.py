f30fa008111ffc1c997990d0e8ccf3f6f9abd819
darren-shen <darrenshen@berkeley.edu>
2024-08-06 23:13:06 -0700

    im scared

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,7 +1,7 @@
 from app.main import app
 from flask_socketio import SocketIO
  
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 if __name__ == "__main__":
     socketio.run(app, port=5001, debug=True)
